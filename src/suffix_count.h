a2fdcb0577e67f8af969635c0f723d35e9935059
Silas-Raye <silas.raye@gmail.com>
2023-05-19 11:52:50 -0400

    modified:   src/suffix_count.h

--- src/suffix_count.h
+++ src/suffix_count.h
@@ -8,7 +8,7 @@ int suffixCount(string S, int L) {
     int nPos = S.find(suffix, 0);
     while (nPos != string::npos) {
         count++;
-        nPos = S.find(suffix, nPos + 1);
+        nPos = S.find(suffix, nPos + 1); // If the suffix is not found, the find function will return string::npos
     }
     return count;
 }
